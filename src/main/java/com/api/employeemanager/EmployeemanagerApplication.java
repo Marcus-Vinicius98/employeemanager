59d97b2b4deb9c479b1ccbc05f373846ab4f3833
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-09-10 17:02:12 -0300

    open api swagger implemented

--- src/main/java/com/api/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/api/employeemanager/EmployeemanagerApplication.java
@@ -1,4 +1,4 @@
-package com.api.employeemanager;
+ package com.api.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/api/employeemanager/configs/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.api.employeemanager.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+	
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+	      return new OpenAPI()
+	              .info(new Info().title("Employee Manager RestFul API")
+	              .description("Employee manager application")
+	              .version("v0.0.1")
+	              .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+	              .externalDocs(new ExternalDocumentation()
+	              .url("https://springshop.wiki.github.org/docs"));
+	  }
+
+}
